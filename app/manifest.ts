8c48bdea76e47af0768126c2f3e4788156e47514
IFOR1607 <3brother089@gmail.com>
2026-08-02 15:38:20 +0700

    feat:add PWA manifest config

---
+++ app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Family Information Center',
+    short_name: 'FIC',
+    description: 'Platform Digital Keluarga Besar',
+    start_url: '/',
+    display: 'standalone', // Ini rahasianya biar ga ada address bar browser!
+    background_color: '#f1f5f9',
+    theme_color: '#059669',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
