be3f7e87a48b81511760b4e1b8eaf0c97d67bf62
IFOR1607 <3brother089@gmail.com>
2026-08-02 15:47:04 +0700

    fix: update high-res PWA icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,9 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#059669',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/logoFamily.jpeg',
+        sizes: '512x512',
+        type: 'image/jpeg',
       },
     ],
   }
