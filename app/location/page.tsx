2ccf9228da8828700a830e41daacbdce7de998ae
IFOR1607 <3brother089@gmail.com>
2026-08-02 23:58:24 +0700

    Menambah detail page location

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,5 +1,5 @@
 import LocationHeader from "@/components/location/LocationHeader";
-import LiveMap from "@/components/maps/LeafletMap";
+import LiveMap from "@/components/maps/LiveMap";
 import FloatingControls from "@/components/location/FloatingControls";
 import FamilyBottomSheet from "@/components/location/FamilyBottomSheet";
 import { LocationProvider } from "@/contexts/LocationContext";
